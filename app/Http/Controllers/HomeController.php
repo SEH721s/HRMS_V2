33841a20529f0f1a6ebed215197be316ad6ad23f
hpmouton <hubertthedev@gmail.com>
2021-11-08 00:31:54 +0200

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index(){
+
+        $role = Auth::user()->role;
+        if($role == 1){
+            return view('hr.dashboard');
+        }
+        elseif($role == 0){
+            return view('hod.dashboard');
+        }
+    }
+}
